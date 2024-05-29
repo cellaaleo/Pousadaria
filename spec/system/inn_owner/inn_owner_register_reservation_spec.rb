b19c46f9529cc84348dbed4b4d62c9f64088deed
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-05-29 19:32:45 +0100

    commit da segunda parte da entrevista

---
+++ spec/system/inn_owner/inn_owner_register_reservation_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Dono de Pousada registra uma reserva" do
+  it "com sucesso" do
+    inn_owner = InnOwner.create!(first_name: 'Joao', last_name: 'Almeida', 
+                                  document: '53783222001', email: 'joao@email.com', password: '123456')
+    inn = inn_owner.create_inn!(name: 'Pousada do Almeidinha', registration_number: '30638898000199', description: 'Um bom lugar', 
+                                address_attributes: { address: 'Rua X', number: '100', city: 'Manaus', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
+
+    login_as inn_owner, scope: :inn_owner
+    visit root_path
+    click_on 'Gestão de Pousadas'
+    click_on 'Cadastrar Reserva'
+    fill_in "name",	with: "Turma Treina Dev 12" 
+    fill_in "starting_date", with: 2.weeks.from_now
+    fill_in "ending_date", with: 3.weeks.from_now
+    click_on 'Cadastrar'
+
+    expect(page).to have_content 'Reserva cadastrada com sucesso!'
+    expect(page).to have_content 'Nome da reserva: Turma Treina Dev 12'
+    expect(page).to have_content 'Início: '
+    expect(page).to have_content 'Fim: '
+  end
+  
+end
