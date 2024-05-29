245c1cc7314ecf867834faf5a383e13c8afdf499
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-05-29 20:05:51 +0100

    commit final

--- app/controllers/inn_dashboard/inn_reservations_controller.rb
+++ app/controllers/inn_dashboard/inn_reservations_controller.rb
@@ -2,4 +2,18 @@ class InnDashboard::InnReservationsController < InnDashboard::InnDashboardContro
   def new
     @inn_reservation = InnReservation.new
   end
+
+  def create
+    @inn_reservation = InnReservation.new(reservation_params)
+
+    if @inn_reservation.save
+      redirect_to @inn_reservation, notice: 'Reserva cadastrada com sucesso!'
+    end
+  end
+  
+
+  private
+  def reservation_params
+    params.require(:inn_reservation).permit(:name, :starting_date, :ending_date)
+  end
 end
